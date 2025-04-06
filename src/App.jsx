cdfde99538162abeae51ee5a1af36f44cb44cf98
magedzidan <magedzidan@yahoo.com>
2025-04-06 06:55:31 +0200

    Add functionality for creating new projects with input fields and styling updates.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
+import { useState } from "react";
+
 function App() {
+  const [newProject,setNewProject]=useState(false);
+  function MakeNewProject(){
+    setNewProject(true);
+  }
+
+  function SaveProject(){
+    setNewProject(false);
+  }
+
+  function CancelProject(){
+    setNewProject(false);
+  }
   return (
     <>
     <div id="ProjectsBar" className="bg-amber-950">
     <h1 className="text-4xl font-bold">Your Projects</h1>
-    <button className="bg-amber-900" >+AddProject</button>
+    <button className="bg-amber-900" onClick={MakeNewProject} >+AddProject</button>
     </div>
-    <div id="emptyBody">
+    {newProject?<div className="newProject">
+
+      <div className="flex flex-row gap-3 self-end">
+      <button className="bg-transparent" onClick={CancelProject} >cancel</button>
+      <button className="bg-amber-900 p-3 newBtn " onClick={SaveProject}>save</button>
+      </div>
+        
+        <div className="input_area flex flex-col justify-start ">
+        <label>Title</label>
+        <input type="text"></input>
+        <label>Descrbtion</label>
+        <input type="text"></input>
+        <label>Due Date</label>
+        <input type="date"></input>
+        </div>
+
+    </div>
+    :<div id="emptyBody">
       <img src="src\assets\no-projects.png" width={80} height={90} alt='Large Pizza'/>
       <h1 className="text-4xl font-bold">No Project Selected</h1>
-      <p className="text-base ">Select a project or get started with a new one</p>
-      <button className="bg-amber-900">Create new project</button>
-    </div>
+      <p className="text-base">Select a project or get started with a new one</p>
+      <button className="bg-amber-900" onClick={MakeNewProject}>Create new project</button>
+    </div>}
+   
       
     </>
   );
