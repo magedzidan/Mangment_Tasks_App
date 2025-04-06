92c0257e60526ad128a3baa5c98c3f842c9f55dc
magedzidan <magedzidan@yahoo.com>
2025-04-06 05:23:52 +0200

    Enhance UI with new project management layout and styles.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 function App() {
   return (
     <>
-      <h1 className="my-8 text-center text-4xl font-bold">Hello World</h1>
+    <div id="ProjectsBar" className="bg-amber-950">
+    <h1 className="text-4xl font-bold">Your Projects</h1>
+    <button className="bg-amber-900" >+AddProject</button>
+    </div>
+    <div id="emptyBody">
+      <img src="src\assets\no-projects.png" width={80} height={90} alt='Large Pizza'/>
+      <h1 className="text-4xl font-bold">No Project Selected</h1>
+      <p className="text-base ">Select a project or get started with a new one</p>
+      <button className="bg-amber-900">Create new project</button>
+    </div>
+      
     </>
   );
 }
